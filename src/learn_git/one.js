20045ad01f6bef5235461d81654f7f44c724ee2a
Shivam Singh <shivam.s@srmtechsol.com>
2026-06-17 15:02:16 +0530

    comming in development branch

---
+++ src/learn_git/one.js
@@ -0,0 +1,15 @@
+const one = () => {
+    console.log("one");
+}
+const two = () => {
+    console.log("two");
+}
+const three = () => {
+    console.log("three");
+}
+const four = () => {
+    console.log("four");
+}
+const five = () => {
+    console.log("five");
+}
